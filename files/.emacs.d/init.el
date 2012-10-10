fb77c3a9c028f783f4f8da7d76639b9d90aa9646
kellydunn <defaultstring@gmail.com>
2012-10-10 14:40:48 -0700

    [files] Adding in package.el as I just learned about this package manager :P

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -2,6 +2,9 @@
 (setq auto-save-default nil)
 (setq-default indent-tabs-mode nil)
 
+(add-to-list 'load-path "~/.emacs.d/")
+(load "package")
+
 (add-to-list 'load-path "~/.emacs.d/modes/")
 (load "haml-mode/haml-mode")
 (load "go-mode/go-mode")
