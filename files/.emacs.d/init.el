f107dbd4365a8b640c30388f678a36bc1c4028c7
kellydunn <defaultstring@gmail.com>
2014-08-20 23:21:03 -0700

    Adding in base-16 emacs themes

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -6,20 +6,24 @@
 (require 'package)
 (package-initialize)
 
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 
 (custom-set-variables
- '(custom-enabled-themes (quote (tomorrow-night-eighties)))
- '(custom-safe-themes 
-   (quote 
-    ("cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" default)))
-
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (base16-chalk)))
+ '(custom-safe-themes (quote ("7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "9bac44c2b4dfbb723906b8c491ec06801feb57aa60448d047dbfdbd1a8650897" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" "de2c46ed1752b0d0423cde9b6401062b67a6a1300c068d5d7f67725adc6c3afb" "023c81cf78bd093a1766026dacf3ae1e6809c1c75a81aa28817e970c048e9719" "781d43bab03fd53b3d89f0fcac15b45a2627b8da16d90545e67f709745d83330" "405fda54905200f202dd2e6ccbf94c1b7cc1312671894bc8eca7e6ec9e8a41a2" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" default)))
+ '(custom-theme-directory "themes")
  '(inhibit-startup-screen t)
  '(initial-scratch-message ";; yo"))
 
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
