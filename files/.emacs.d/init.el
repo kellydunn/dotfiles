2590309d6fa9c54f1b36c458f9db5e86c63a1d68
kellydunn <defaultstring@gmail.com>
2014-08-21 00:38:13 -0700

    Adding some new themes

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -18,8 +18,8 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#282a2e" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#e0e0e0"])
  '(ansi-term-color-vector [unspecified "#282a2e" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#e0e0e0"])
- '(custom-enabled-themes (quote (base16-eighties)))
- '(custom-safe-themes (quote ("023c81cf78bd093a1766026dacf3ae1e6809c1c75a81aa28817e970c048e9719" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" default)))
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes (quote ("c7471ce3bb42defac344b3ecfca74228731b5ab20f804fd1deb8e65dddeab26a" "1b64dc598c399356e04dac4de6427e4b04b8528bec34217d0d441fd7453ba7e9" "9b41f298ad28dc56765b227e4b9ed38f98a236706a3a26b148491a0dade90568" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "dc758223066a28f3c6ef6c42c9136bf4c913ec6d3b710794252dc072a3b92b14" "6718a84305356c0f4f53c26dfb87dc3997df22b1fbc5bc483445f7cd7eecac4d" "9bac44c2b4dfbb723906b8c491ec06801feb57aa60448d047dbfdbd1a8650897" "023c81cf78bd093a1766026dacf3ae1e6809c1c75a81aa28817e970c048e9719" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" default)))
  '(inhibit-startup-screen t)
  '(initial-scratch-message ";; yo"))
 
